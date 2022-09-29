84f7f548215986d87a68ef948ec4938ceca615cd
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-09-29 18:12:20 -0300

    Remove espaços em branco

--- MonsterTown/MonsterTown/Monster.swift
+++ MonsterTown/MonsterTown/Monster.swift
@@ -10,7 +10,7 @@ import Foundation
 class Monster {
     var town: Town?
     var name = "Monster"
-    
+
     func terrorizeTown() {
         if town != nil {
             print("\(name) is terrorizing a town!")
--- MonsterTown/MonsterTown/Town.swift
+++ MonsterTown/MonsterTown/Town.swift
@@ -10,11 +10,11 @@ import Foundation
 struct Town {
     var population = 5_422
     var numberOfStoplights = 4
-    
+
     func printDescription() {
         print("Population: \(population); number of stoplights: \(numberOfStoplights)")
     }
-    
+
     mutating func changePopulation(by amount: Int) {
         population += amount
     }
