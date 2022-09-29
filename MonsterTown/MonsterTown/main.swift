8a8462b5d986e5b760583a0b2e24e0c0462c79f2
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-09-29 18:14:46 -0300

    refactor

--- MonsterTown/MonsterTown/main.swift
+++ MonsterTown/MonsterTown/main.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 var myTown = Town()
-myTown.changePopulation(by: 500)
-myTown.printDescription()
 
-let genericMonster = Monster()
-genericMonster.town = myTown
-genericMonster.terrorizeTown()
+myTown.changePopulation(by: 500)
+
+let fredTheZombie = Zombie()
+
+fredTheZombie.town = myTown
+fredTheZombie.terrorizeTown()
+fredTheZombie.town?.printDescription()
