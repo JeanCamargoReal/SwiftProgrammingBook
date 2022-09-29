c47f61186d6723ebdcc52a589818dc1a75bf5e27
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-09-29 15:06:47 -0300

    Instacia classe Monter

--- MonsterTown/MonsterTown/main.swift
+++ MonsterTown/MonsterTown/main.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 var myTown = Town()
-
 myTown.changePopulation(by: 500)
 myTown.printDescription()
+
+let genericMonster = Monster()
+genericMonster.town = myTown
+genericMonster.terrorizeTown()
