f288fc7474f0b2963a79db8eebe04194653c7306
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-09-29 15:05:10 -0300

    Cria classe de Zombie herdando de Monster

--- MonsterTown/MonsterTown/Zombie.swift
+++ MonsterTown/MonsterTown/Zombie.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+class Zombie: Monster {
+    var walksWithLimp = true
+    
+    override func terrorizeTown() {
+        town?.changePopulation(by: -10)
+        
+        super.terrorizeTown()
+    }
+}
