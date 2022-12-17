33aaec8908d3cc9fd41da556f53002680073cc6d
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-12-17 19:17:55 -0300

    Using a convenience initializer

--- MonsterTown/MonsterTown/Zombie.swift
+++ MonsterTown/MonsterTown/Zombie.swift
@@ -22,7 +22,16 @@ class Zombie: Monster {
     init(limp: Bool, fallingApart: Bool, town: Town?, monsterName: String) {
         walksWithLimp = limp
         isFallingApart = fallingApart
+
         super.init(town: town, monsterName: monsterName)
+    }
+
+    convenience init(limp: Bool, fallingApart: Bool) {
+        self.init(limp: limp, fallingApart: fallingApart, town: nil, monsterName: "Fred")
+
+        if walksWithLimp {
+            print("This zombie has a had knee.")
+        }
     }
 
     func regenerate() {
