4d61d05c46f30dd7bbecc9b1b909a90ebf0670e4
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-03-22 12:00:28 -0300

    Fred, we hardly knew ye

--- MonsterTown/MonsterTown/main.swift
+++ MonsterTown/MonsterTown/main.swift
@@ -17,25 +17,25 @@ print(myTownSize)
 myTown.changePopulation(by: 1_000_000)
 print("Size: \(myTown.townSize); population: \(myTown.population)")
 
-let fredTheZombie = Zombie(limp: false,
-                           fallingApart: false,
-                           town: myTown,
-                           monsterName: "Fred")
-
-fredTheZombie.town = myTown
-fredTheZombie.terrorizeTown()
-fredTheZombie.town?.printDescription()
+var fredTheZombie: Zombie? = Zombie(limp: false,
+									fallingApart: false,
+									town: myTown,
+									monsterName: "Fred")
+fredTheZombie?.terrorizeTown()
+fredTheZombie?.town?.printDescription()
 
 var convenientZombie = Zombie(limp: true, fallingApart: false)
 
-print("Victim pool: \(fredTheZombie.victimPool)")
+print("Victim pool: \(String(describing: fredTheZombie?.victimPool))")
 
-fredTheZombie.victimPool = 500
+fredTheZombie?.victimPool = 500
 
-print("Victim pool: \(fredTheZombie.victimPool)")
+print("Victim pool: \(String(describing: fredTheZombie?.victimPool))")
 
 print(Zombie.spookyNoise)
 
 if Zombie.isTerrifying {
     print("Run away!")
 }
+
+fredTheZombie = nil
