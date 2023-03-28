453c26d9392269c85e4c8036897419b1a4bdef90
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-03-28 10:02:05 -0300

    Setting up a table

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func printTable(_ data: [[String]]) {
+	for row in data {
+		// Start the output string
+		var out = "|"
+
+		// Append each item in this row to the string
+		for item in row {
+			out += " \(item) |"
+		}
+
+		// Done - print int!
+		print(out)
+	}
+}
+
+let data = [
+	["Eva", "30", "6"],
+	["Saleh", "40", "18"],
+	["Amit", "50", "20"]
+]
+
+printTable(data)
