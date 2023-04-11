7d3a75bb037f0dc1088d86f368b8450943229b25
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-04-11 16:56:56 -0300

    Printing Department

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -97,3 +97,5 @@ var department = Department(name: "Engineering")
 department.add(Person(name: "Eva", age: 30, yearsOfExperience: 6))
 department.add(Person(name: "Saleh", age: 40, yearsOfExperience: 18))
 department.add(Person(name: "Amit", age: 50, yearsOfExperience: 6))
+
+printTable(department)
