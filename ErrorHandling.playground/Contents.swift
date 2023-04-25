a4d57647249b019aac0b732ba2641177684c477b
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-04-25 14:01:08 -0300

    Declaring the Token type and adding a debug description

--- ErrorHandling.playground/Contents.swift
+++ ErrorHandling.playground/Contents.swift
@@ -1,3 +1,15 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+enum Token: CustomStringConvertible {
+	case number(Int)
+	case plus
+
+	var description: String {
+		switch self {
+			case .number(let n):
+				return "Number: \(n)"
+			case .plus:
+				return "Symbol: +"
+		}
+	}
+}
