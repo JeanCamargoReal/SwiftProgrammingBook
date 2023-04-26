8f68c3996cb53a809586564e391e3729af30ef09
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-04-26 11:17:46 -0300

    Declaring the throwing lex() method

--- ErrorHandling.playground/Contents.swift
+++ ErrorHandling.playground/Contents.swift
@@ -32,7 +32,32 @@ class Lexer {
 
 	func advance() {
 		assert(position < input.endIndex, "Cannot advance past endIndex!")
-		
+
 		position = input.index(after: position)
+	}
+
+	func lex() throws -> [Token] {
+		var tokens = [Token]()
+
+		while let nextCharacter = peek() {
+			switch nextCharacter {
+				case "0" ... "9":
+					// Start of a number - need to grab the rest
+					break // TODO: replace this with real work
+
+				case "+":
+					tokens.append(.plus)
+					advance()
+
+				case " ":
+					// Just advance to ignore spaces
+					advance()
+
+				default:
+					// Something unexpected - need to send back an error
+					break // TODO: replace this with real work
+			}
+		}
+		return tokens
 	}
 }
