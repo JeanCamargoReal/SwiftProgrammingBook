e8d29c437cb8034487cb7c8728bbfc2a3f43aec7
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-05-02 11:32:53 -0300

    Beginning the implementation of Parser; Defining possible Parser errors; Implementing Parser.getNumber()

--- ErrorHandling.playground/Contents.swift
+++ ErrorHandling.playground/Contents.swift
@@ -89,6 +89,46 @@ class Lexer {
 	}
 }
 
+class Parser {
+	enum Error: Swift.Error {
+		case unexpectedEndOfInput
+		case invalidToken(Token)
+	}
+
+	let tokens: [Token]
+	var position = 0
+
+	init(tokens: [Token]) {
+		self.tokens = tokens
+	}
+
+	func getNextToken() -> Token? {
+		guard position < tokens.count else {
+			return nil
+		}
+
+		let token = tokens[position]
+		position += 1
+
+		return token
+	}
+
+	func getNumber() throws -> Int {
+		guard let token = getNextToken() else {
+			throw Parser.Error.unexpectedEndOfInput
+		}
+
+		switch token {
+			case .number(let value):
+				return value
+			case .plus:
+				throw Parser.Error.invalidToken(token)
+		}
+	}
+
+	
+}
+
 func evaluate(_ input: String) {
 	print("Evaluating: \(input)")
 
