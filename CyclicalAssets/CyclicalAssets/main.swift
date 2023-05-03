ed676940a305724fff40cd887233166837ed609b
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-05-03 11:07:25 -0300

    Vault 13 is storing assets

--- CyclicalAssets/CyclicalAssets/main.swift
+++ CyclicalAssets/CyclicalAssets/main.swift
@@ -17,6 +17,9 @@ class Simulation {
 		let gem: Asset = Asset(name: "Big Diamond", value: 5_000.0)
 		let poem: Asset = Asset(name: "Magnum Opus", value: 0.0)
 
+		vault13.store(coin)
+		vault13.store(gem)
+
 		print("Created some assets: \([coin, gem, poem])")
 	}
 }
