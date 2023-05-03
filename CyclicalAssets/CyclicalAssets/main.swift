9895b1e31ed6d4f2a79c01eadafc0be08c4be27c
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-05-03 10:51:43 -0300

    Creating assets

--- CyclicalAssets/CyclicalAssets/main.swift
+++ CyclicalAssets/CyclicalAssets/main.swift
@@ -12,6 +12,12 @@ class Simulation {
 		let vault13 = Vault(number: 13)
 
 		print("Created \(vault13)")
+
+		let coin: Asset = Asset(name: "Rare Coin", value: 1_000.0)
+		let gem: Asset = Asset(name: "Big Diamond", value: 5_000.0)
+		let poem: Asset = Asset(name: "Magnum Opus", value: 0.0)
+
+		print("Created some assets: \([coin, gem, poem])")
 	}
 }
 
