a4cbfd69103b7b580ec28846bdebb7d4655fe85f
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-05-08 08:52:13 -0300

    Defining Point

--- Comparison.playground/Contents.swift
+++ Comparison.playground/Contents.swift
@@ -1,3 +1,11 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct Point: {
+	let x: Int
+	let y: Int
+	let label: String? = nil
+}
+
+let a = Point(x: 3, y: 4)
+let b = Point(x: 3, y: 5)
+let abEqual = (a == b)
