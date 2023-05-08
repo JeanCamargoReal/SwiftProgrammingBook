0d612ca1db003cd44e6ccf8b6c9921691f411564
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-05-08 14:18:42 -0300

    Testing the < function

--- Comparison.playground/Contents.swift
+++ Comparison.playground/Contents.swift
@@ -10,6 +10,10 @@ let a = Point(x: 3, y: 4)
 let b = Point(x: 3, y: 4)
 let abEqual = (a == b)
 let abNotEqual = (a != b)
+let c = Point(x: 2, y: 6)
+let d = Point(x: 3, y: 7)
+let cdEqual = (c == d)
+let cLessThanD = (c < d)
 
 extension Point: Equatable, Comparable {
 
